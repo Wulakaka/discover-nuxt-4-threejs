a9b49d0051359b0fd62af612dc7e8ca6d6b88909
zhang.weifeng <zhangweifeng@diinom.com>
2026-01-14 11:02:52 +0800

    feat(pcd loader): 增加测试代码

---
+++ app/pages/pcd/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {OrbitControls, Align} from "@tresjs/cientos";
+import {PCDLoader} from "three/examples/jsm/Addons.js";
+
+const {state, error, isLoading} = useLoader(PCDLoader, "/pcd/test.pcd");
+</script>
+
+<template>
+  <div>
+    <TresCanvas window-size>
+      <TresPerspectiveCamera :position="[0, 0, 1]" :near="0.01" :far="100" />
+      <OrbitControls />
+      <TresAmbientLight :intensity="0.5" />
+      <TresDirectionalLight :position="[3, 2, 1]" />
+      <Align v-if="state" :center="[0, 0, 0]">
+        <primitive
+          :object="state"
+          :scale="0.001"
+          :rotation-x="-Math.PI / 2"
+          dispose
+        />
+      </Align>
+    </TresCanvas>
+    <div class="absolute inset-0 z-10 text-white">
+      error: {{ error }} isLoading: {{ isLoading }}
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,5 +14,6 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@vueuse/nuxt",
     "motion-v/nuxt",
+    "@tresjs/nuxt",
   ],
 });
