8f7cfbbb6fc4303c879c6dd5d66fde95c280140e
zhang.weifeng <zhangweifeng@diinom.com>
2026-02-28 16:43:46 +0800

    feat(Line): 增加移动虚线

---
+++ app/pages/line/components/MovingLine.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import pointList from "@/assets/json/points.json";
+import {Line2} from "@tresjs/cientos";
+
+const positions = pointList.flatMap((p) => [p.x, p.y, p.z]);
+
+const dashOffset = ref(0);
+
+const {onRender} = useLoop();
+
+onRender(({delta}) => {
+  dashOffset.value -= delta * 5;
+});
+</script>
+
+<template>
+  <Line2
+    :points="positions"
+    color="orange"
+    :line-width="5"
+    dashed
+    :gap-size="0.2"
+    :dash-offset="dashOffset"
+    alpha-to-coverage
+  />
+</template>
+
+<style scoped></style>
---
+++ app/pages/line/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {OrbitControls} from "@tresjs/cientos";
+import MovingLine from "./components/MovingLine.vue";
+</script>
+
+<template>
+  <div class="overflow-hidden">
+    <TresCanvas window-size>
+      <TresPerspectiveCamera :position="[0, 0, 10]" :near="0.01" :far="100" />
+      <OrbitControls />
+      <MovingLine />
+    </TresCanvas>
+  </div>
+</template>
+
+<style scoped></style>
