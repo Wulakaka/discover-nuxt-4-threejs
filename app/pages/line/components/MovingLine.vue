2e862f2a5309f11613e21db3b932d785f0fddb81
zhang.weifeng <zhangweifeng@diinom.com>
2026-03-03 10:53:38 +0800

    feat(line): 增加从 csv 读取数据逻辑

--- app/pages/line/components/MovingLine.vue
+++ app/pages/line/components/MovingLine.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts">
-import {Line2} from "@tresjs/cientos";
+<script setup lang="ts" async>
+import {Align, Line2} from "@tresjs/cientos";
 import {CatmullRomCurve3, Vector3} from "three";
-import pointList from "@/assets/json/points.json";
 
-const points = pointList.map((p) => new Vector3(p.x, p.y, p.z));
+const props = defineProps<{
+  data: {x: string; y: string; z: string}[];
+}>();
+
+const points = props.data.map((p) => new Vector3(+p.x, +p.y, +p.z));
+
 const curve = new CatmullRomCurve3(points);
-const linePoints = curve.getPoints(pointList.length * 10);
+const linePoints = curve.getPoints(props.data.length * 10);
 
 const positions = linePoints.flatMap((p) => [p.x, p.y, p.z]);
 
@@ -15,9 +19,9 @@ const {onRender} = useLoop();
 const dashOffset = ref(0);
 
 onRender(({elapsed, delta}) => {
-  dashOffset.value -= delta * 10;
+  dashOffset.value -= delta * 100;
   if (arrowRef.value) {
-    const t = (elapsed * 0.01) % 1;
+    const t = (elapsed * 0.1) % 1;
     const position = curve.getPointAt(t);
     const tangent = curve.getTangentAt(t).normalize();
     arrowRef.value.position.copy(position);
@@ -29,15 +33,15 @@ onRender(({elapsed, delta}) => {
 <template>
   <TresGroup>
     <Line2
-      :points="positions"
+      :points="points"
       color="orange"
       :line-width="5"
       dashed
-      :gap-size="0.2"
+      :gap-size="1"
       :dash-offset="dashOffset"
     />
     <TresGroup ref="arrowRef">
-      <TresMesh :rotation="[Math.PI / 2, 0, 0]">
+      <TresMesh :rotation="[Math.PI / 2, 0, 0]" :scale="5">
         <TresConeGeometry :args="[0.1, 0.4, 4]" />
         <TresMeshBasicMaterial color="white" />
       </TresMesh>
--- app/pages/line/index.vue
+++ app/pages/line/index.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
 import {OrbitControls} from "@tresjs/cientos";
 import MovingLine from "./components/MovingLine.vue";
+import csvFile from "@/assets/csv/trajectory.csv?url";
+import * as d3 from "d3";
+
+const data = ref<{x: string; y: string; z: string}[]>();
+d3.csv<"x" | "y" | "z">(csvFile).then((csvData) => {
+  data.value = csvData;
+});
 </script>
 
 <template>
   <div class="overflow-hidden">
     <TresCanvas window-size>
-      <TresPerspectiveCamera :position="[0, 0, 10]" :near="0.01" :far="100" />
+      <TresPerspectiveCamera
+        :position="[0, 100, 100]"
+        :near="0.01"
+        :far="1000"
+      />
       <OrbitControls />
-      <MovingLine />
+      <MovingLine v-if="data" :data="data" />
     </TresCanvas>
   </div>
 </template>
