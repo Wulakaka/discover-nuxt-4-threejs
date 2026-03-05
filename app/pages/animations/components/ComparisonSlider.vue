6be8b102ca8d7ba31f8899ddbfc102bbb01aeace
zhang.weifeng <zhangweifeng@diinom.com>
2026-03-05 18:00:03 +0800

    feat(animations): 调整对比滑块逻辑

--- app/pages/animations/components/ComparisonSlider.vue
+++ app/pages/animations/components/ComparisonSlider.vue
@@ -11,19 +11,19 @@ const {x, style} = useDraggable(el, {
 const percent = computed(() => {
   if (!parent.value) return 0;
   const parentRect = parent.value.getBoundingClientRect();
-  return (1 - x.value / parentRect.width) * 100;
+  return (1 - x.value / (parentRect.width - 8)) * 100;
 });
 </script>
 
 <template>
-  <div ref="parent" class="relative pr-2 text-6xl">
+  <div ref="parent" class="relative px-1 text-6xl">
     <div
       class="w-200 h-140 bg-amber-200 flex justify-center items-center text-blue-300"
     >
       {{ x }}
     </div>
     <div
-      class="absolute inset-0 bg-blue-300 flex justify-center items-center text-amber-200"
+      class="absolute inset-x-1 inset-y-0 bg-blue-300 flex justify-center items-center text-amber-200"
       :style="{
         'clip-path': `inset(0% ${percent}% 0% 0%)`,
       }"
@@ -32,7 +32,7 @@ const percent = computed(() => {
     </div>
     <div
       ref="el"
-      class="absolute h-full w-2 bg-gray-600 opacity-25 hover:opacity-100 cursor-col-resize transition-opacity duration-150 -translate-x-1/2"
+      class="absolute h-full w-2 bg-gray-600 opacity-10 hover:opacity-100 cursor-col-resize transition-opacity duration-150"
       :style="style"
     />
   </div>
