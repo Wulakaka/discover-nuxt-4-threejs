56cdabe6c27ac71bb23aac2c154fea9d3738cd3b
zhang.weifeng <zhangweifeng@diinom.com>
2026-07-01 17:02:54 +0800

    feat(raymarching): 增加反射效果

--- app/pages/raymarching/shaders/fragment.glsl
+++ app/pages/raymarching/shaders/fragment.glsl
@@ -1,4 +1,5 @@
 uniform vec2 uResolution;
+uniform float uTime;
 
 #define MAX_STEPS 100
 #define MAX_DIST 100.0
@@ -30,12 +31,24 @@ float raymarch(vec3 ro, vec3 rd) {
   return dO;
 }
 
+// 根据 Inigo Quilez 的 https://iquilezles.org/articles/normalsSDF/
+vec3 getNormal(vec3 p) {
+  vec2 e = vec2(.01, 0);
+
+  vec3 n = scene(p) - vec3(scene(p - e.xyy), scene(p - e.yxy), scene(p - e.yyx));
+
+  return normalize(n);
+}
+
 void main() {
 
   // 将uv中心变为(0,0)，y轴范围为[-1,1]，x轴范围为[-aspect, aspect]
   vec2 uv = gl_FragCoord.xy / uResolution.xy;
   uv -= 0.5;
   uv.x *= uResolution.x / uResolution.y;
+
+  // Light Position
+  vec3 lightPosition = vec3(-10.0 * cos(uTime), 10.0, 10.0 * sin(uTime));
 
   // Ray Origin - camera
   vec3 ro = vec3(0.0, 0.0, 5.0);
@@ -47,7 +60,12 @@ void main() {
 
   vec3 color = vec3(0.0);
   if(d < MAX_DIST) {
-    color = vec3(1.0);
+    vec3 normal = getNormal(p);
+    vec3 lightDirection = normalize(lightPosition - p);
+
+    float diffuse = max(dot(normal, lightDirection), 0.0);
+
+    color = vec3(1.0) * diffuse;
   }
 
   gl_FragColor = vec4(color, 1.0);
